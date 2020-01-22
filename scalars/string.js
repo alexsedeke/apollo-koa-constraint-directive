c0fdb50f367b45071aee803e182755eb7fc53648
Alexander Vu <alexander.vu.tuyet@gmail.com>
2020-01-22 23:50:49 +0100

    Fix password score error message

--- scalars/string.js
+++ scalars/string.js
@@ -90,6 +90,6 @@ function validate (fieldName, args, value) {
   if (args.passwordScore && !password(value, args.passwordScore)) {
     throw new ValidationError(fieldName,
       `Password for ${fieldName} is not strong enough to reach a score of ${args.passwordScore}`,
-      [{ arg: 'password', value: args.password }])
+      [{ arg: 'password', value: args.passwordScore }])
   }
 }
